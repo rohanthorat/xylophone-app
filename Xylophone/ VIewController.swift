a9f8287267299ff2cdaef00874b2510cd16defc0
Rohan Thorat <rthorat1@binghamton.edu>
2018-05-27 22:21:06 -0400

    sound initiation

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,10 +7,14 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController{
     
+    var player: AVAudioPlayer?
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -19,7 +23,17 @@ class ViewController: UIViewController{
 
     @IBAction func notePressed(_ sender: UIButton) {
         
+        let url = Bundle.main.url(forResource: "note1", withExtension: "wav")!
         
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            guard let player = player else { return }
+            
+            player.prepareToPlay()
+            player.play()
+        } catch let error as NSError {
+            print(error)
+        }
         
     }
     
